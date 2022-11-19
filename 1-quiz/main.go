905e9bcdbdb71dc650966b7ae6c6f7a07a906ff3
Julian Chong <26028372+julianchong00@users.noreply.github.com>
2022-11-19 20:11:59 +1100

    feat(quiz): quiz works as intended

--- 1-quiz/main.go
+++ 1-quiz/main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -81,25 +82,28 @@ func main() {
 	fmt.Print("Press enter to start the quiz...")
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 
+	// Keep track of score
+	score := 0
+
 	// Start timer
+	// Timer sends a message on the channel after the specified duration
 	timer := time.NewTimer(time.Duration(config.TimeLimit) * time.Second)
 	go func() {
 		// Block until the timer receives a message on its channel after time limit
 		<-timer.C
-		fmt.Println("Time's up!")
-        os.Exit(0)
+		fmt.Println("\nTime's up!")
+		fmt.Printf("You scored %d out of %d.\n", score, len(problems))
+		os.Exit(0)
 	}()
-
-	score := 0
 
 	reader := bufio.NewReader(os.Stdin)
 	for i, problem := range problems {
-		fmt.Scanf("Problem #%d: %s = ", i, problem.Question)
+		fmt.Printf("Problem #%d: %s = ", i, problem.Question)
 		answer, _ := reader.ReadString('\n')
 
-		if answer == problem.Answer {
+		if strings.TrimSpace(answer) == problem.Answer {
 			score++
 		}
 	}
-    fmt.Printf("You scored %d out of %d", score, len(problems))
+	fmt.Printf("You scored %d out of %d.\n", score, len(problems))
 }
