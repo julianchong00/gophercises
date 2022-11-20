ed02db0b29ee817915d95962102b1109b42b9360
Julian Chong <26028372+julianchong00@users.noreply.github.com>
2022-11-20 22:25:05 +1100

    feat(cyoa): handled url paths which mapped to different chapters in the story and styled html

--- 3-cyoa/story.go
+++ 3-cyoa/story.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -22,15 +24,57 @@ var defaultHandlerTemplate = `
         <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body>
-        <h1>{{.Title}}</h1>
-        {{range .Paragraphs}}
-        <p>{{.}}</p>
-        {{end}}
-        <ul>
-            {{range .Options}}
-                <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        <section class="page">
+            <h1>{{.Title}}</h1>
+            {{range .Paragraphs}}
+            <p>{{.}}</p>
             {{end}}
-        </ul>
+            <ul>
+                {{range .Options}}
+                    <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+                {{end}}
+            </ul>
+            <style>
+                body {
+                    font-family: helvetica, arial;
+                }
+                h1 {
+                    text-align: center;
+                    position: relative;
+                }
+                .page {
+                    width: 80%;
+                    max-width: 500px;
+                    margin: auto;
+                    margin-top: 40px;
+                    margin-bottom: 40px;
+                    padding: 80px;
+                    background: #FFFCF6;
+                    border: 1px solid #eee;
+                    box-shadow: 0 10px 6px -6px #777;
+                }
+                ul {
+                    border-top: 1px dotted #ccc;
+                    padding: 10px 0 0 0;
+                    -webkit-padding-start: 0;
+                }
+                li {
+                    padding-top: 10px;
+                }
+                a,
+                a:visited {
+                    text-decoration: none;
+                    color:#6295b5;
+                }
+                a:active,
+                a:hover {
+                    color: #7792a2;
+                }
+                p {
+                    text-indent: 1em;
+                }
+            </style>
+        </section>
     </body>
     </html>`
 
@@ -43,10 +87,30 @@ type handler struct {
 }
 
 func (h handler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-    err := tpl.Execute(rw, h.s["intro"])
-    if err != nil {
-        panic(err)
-    }
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		// When no path, assume starting story from the intro
+		path = "/intro"
+	}
+	// "/intro" => "intro" - remove slashes
+	path = path[1:]
+
+	// path = ["intro"]
+	// this if statement checks if the map has something in it with the
+	// "ok" variable, otherwise it might still enter conditional without
+	// anything inside the map
+
+    // checks if chapter link exists in map, then writes template to rw
+    // with template.Execute
+	if chapter, ok := h.s[path]; ok {
+		err := tpl.Execute(rw, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(rw, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(rw, "Chapter not found.", http.StatusNotFound)
 }
 
 func JsonStory(r io.Reader) (Story, error) {
