61e859172ce29a6877c8a2ade03759c7180c7385
Julian Chong <26028372+julianchong00@users.noreply.github.com>
2022-11-20 22:02:33 +1100

    feat(cyoa): implemented html template to display story and started creating Handler

--- 3-cyoa/cmd/cyoaweb/main.go
+++ 3-cyoa/cmd/cyoaweb/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/julianchong00/cyoa"
@@ -10,6 +12,7 @@ import (
 
 // Use this file to run web application
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	file := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %v.\n", *file)
@@ -24,5 +27,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- 3-cyoa/story.go
+++ 3-cyoa/story.go
@@ -2,8 +2,52 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTemplate = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Choose Your Own Adventure</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+        <p>{{.}}</p>
+        {{end}}
+        <ul>
+            {{range .Options}}
+                <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+            {{end}}
+        </ul>
+    </body>
+    </html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+    err := tpl.Execute(rw, h.s["intro"])
+    if err != nil {
+        panic(err)
+    }
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
