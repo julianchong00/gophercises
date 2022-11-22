8f83d7882ea76fa69b8f271a8234bf7cc0d44dbc
Julian Chong <26028372+julianchong00@users.noreply.github.com>
2022-11-22 19:35:27 +1100

    feat(link): initial commit for exercise 4 - HTML link parser

---
+++ 4-link/examples/ex1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/julianchong00/link"
+)
+
+var exampleHTML = `
+<html>
+
+<body>
+    <h1>Hello!</h1>
+    <a href="/other-page">A link to another page</a>
+</body>
+
+</html>
+`
+
+func main() {
+	// strings.NewReader returns a reader from a string input
+	r := strings.NewReader(exampleHTML)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
---
+++ 4-link/go.mod
@@ -0,0 +1,5 @@
+module github.com/julianchong00/link
+
+go 1.19
+
+require golang.org/x/net v0.2.0 // indirect
