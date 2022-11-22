5dbc170ad3b6b70c6546b488a9998699f2dc6c04
Julian Chong <26028372+julianchong00@users.noreply.github.com>
2022-11-22 19:57:38 +1100

    feat(link): implemented functions to build a list of Links and recursively extract text from <a>tags

--- 4-link/parse.go
+++ 4-link/parse.go
@@ -1,8 +1,8 @@
 package link
 
 import (
-	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -20,17 +20,56 @@ func Parse(r io.Reader) ([]Link, error) {
 	if err != nil {
 		return nil, err
 	}
-	dfs(doc, "")
-	return nil, nil
+	nodes := linkNodes(doc)
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+	}
+
+	return links, nil
 }
 
-func dfs(n *html.Node, padding string) {
-    msg := n.Data
-    if n.Type == html.ElementNode {
-        msg = "<" + msg + ">"
-    }
-	fmt.Println(padding, msg)
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        dfs(c, padding + "  ")
-    }
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = text(n)
+	return ret
+}
+
+// DFS function to extract text from <a> tags since they can also contain other nodes
+// or even comments (which we want to ignore)
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c) + " "
+	}
+    return strings.Join(strings.Fields(ret), " ")
+}
+
+// DFS function to get all link nodes from HTML and return a list
+func linkNodes(n *html.Node) []*html.Node {
+	// Base case - checks if node is an ElementNode and if it is an <a> tag
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		// Another variadic parameter:
+		// ... after linkNodes(c) basically expands the list that is returned
+		// from deeper iterations
+		ret = append(ret, linkNodes(c)...)
+	}
+
+	return ret
 }
